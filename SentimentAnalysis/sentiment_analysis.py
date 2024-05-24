03adf836bb2e2c70bc97833f67a24305a15f088f
Vishal Patel <77964074+Vishal26199@users.noreply.github.com>
2024-05-24 09:14:55 -0400

    updated

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,5 +1,5 @@
 
-import requests
+import requests,json
 
 def sentiment_analyzer(text_to_analyse):
 
@@ -11,5 +11,8 @@ def sentiment_analyzer(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     
     response = requests.post( url, json = myobj, headers = header )
+    formatted_response = json.loads(response.text)
+    label = formatted_response['documentSentiment']['label']
+    score = formatted_response['documentSentiment']['score']
 
-    return response.text
+    return {'label' : label, 'score' : score}
